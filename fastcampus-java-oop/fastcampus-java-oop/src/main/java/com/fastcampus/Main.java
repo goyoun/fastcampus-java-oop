153f22b392c1c78c28dd19e6d8abf123134221e8
go <monologuess1548@gmail.com>
2022-10-05 16:25:52 +0900

    12
    12

--- fastcampus-java-oop/fastcampus-java-oop/src/main/java/com/fastcampus/Main.java
+++ fastcampus-java-oop/fastcampus-java-oop/src/main/java/com/fastcampus/Main.java
@@ -1,14 +1,15 @@
 package com.fastcampus;
 
 import com.fastcampus.logic.BubbleSort;
+import com.fastcampus.logic.JavaSort;
+import com.fastcampus.logic.Sort;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
         System.out.println("[result] " + sort.sort(Arrays.asList()));
 
     }
